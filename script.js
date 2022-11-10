fd24bfeb2760346bf6f6aad10f9aee733e9d2d63
BuiBao3103 <hongbao2003@gmail.com>
2022-11-10 11:23:55 +0700

    fix headline

--- script.js
+++ script.js
@@ -32,3 +32,17 @@ function choose_size(nth){
     sizes[nth].style.border = "1px solid #000"
     document.querySelector(".choose_size").style.display = "none"
 }
+
+let slideIndex = 0;
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
