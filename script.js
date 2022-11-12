66e0970a77b004886605fc38d076c1a92bd5d248
Bui Ngoc Thuc <buingocthuc1512@gmail.com>
2022-11-12 09:47:51 +0700

    new

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //category
 
-function goToProInfo(img){
+function goToProInfo(img) {
     var category = document.querySelector("#wrapper")
     var product_info = document.querySelector("#product-information")
     category.style.display = "none"
@@ -9,23 +9,23 @@ function goToProInfo(img){
     img_main.src = img.src
 }
 //product-info
-function change_info(nth){
+function change_info(nth) {
     var info_titles = document.querySelectorAll(".product-info-title > *")
-    for(var i = 0; i < info_titles.length; i++)
+    for (var i = 0; i < info_titles.length; i++)
         info_titles[i].style.border = "none"
     info_titles[nth].style.borderBottom = "3px solid #9F7150"
     var infos = document.querySelectorAll(".product-info-content > *")
-    for(var i = 0; i < infos.length; i++)
+    for (var i = 0; i < infos.length; i++)
         infos[i].style.display = "none"
     infos[nth].style.display = "block"
 }
-function change_price(quantity){
+function change_price(quantity) {
     var price_label = document.querySelector(".product-price")
     var price = quantity.value * 390000
     var price_str = price.toLocaleString("en-US");
     price_label.innerHTML = `<p style="color: red;">${price_str} VND</p>`
 }
-function choose_size(nth){
+function choose_size(nth) {
     var sizes = document.querySelectorAll(".size > *")
     for (var i = 0; i < sizes.length; i++)
         sizes[i].style.border = "none"
@@ -36,15 +36,15 @@ function choose_size(nth){
 let slideIndex = 0;
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
 
 var currentPageProduct = 1;
@@ -59,7 +59,7 @@ function showCurrentProduct() {
     productContainer.innerHTML = ``
     for (var i = (currentPageProduct - 1) * 8; i < currentPageProduct * 8; i++) {
         productContainer.innerHTML +=
-        `<li>
+            `<li>
             <div class="product-item">
                 <div class="product-top">
                     <div href="" class="product-thumb">
@@ -84,7 +84,7 @@ function showCurrentDot() {
     paginationContainer.innerHTML = ``
     paginationContainer.innerHTML += `<a onclick="leftPagination(this)">«</a>`
     for (var i = 1; i <= nDot; i++)
-        paginationContainer.innerHTML +=`<a onclick="changePagination(this)">${i}</a>`
+        paginationContainer.innerHTML += `<a onclick="changePagination(this)">${i}</a>`
     paginationContainer.innerHTML += `<a onclick="rightPagination(this)">»</a>`
     var dots = document.querySelectorAll(".pagination > *")
     dots[currentPageProduct].classList.add("active")
@@ -110,3 +110,26 @@ function rightPagination() {
     showCurrentDot()
     showCurrentProduct()
 }
+
+mouse-move
+$(function () {
+    var zoom = function (elm) {
+        elm
+            .on('mouseover', function () {
+                $(this).children('.img').css({ 'transform': 'scale(2)' });
+            })
+            .on('mouseout', function () {
+                $(this).children('.img').css({ 'transform': 'scale(1)' });
+            })
+            .on('mousemove', function (e) {
+                $(this).children('.img').css({ 'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 + '%' });
+            })
+    }
+
+    $('.item').each(function () {
+        $(this)
+            .append('<div class="img"></div>')
+            .children('.img').css({ 'background-image': 'url(' + $(this).data('image') + ')' });
+        zoom($(this));
+    })
+})
